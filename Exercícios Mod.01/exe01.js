f281b68782c74c74b92cdcd080a6aba02245b1d5
JairNeto1 <jairmonteironeto@gmail.com>
2021-06-26 14:24:38 -0300

    fisrst commit

---
+++ Exercícios Mod.01/exe01.js
@@ -0,0 +1,76 @@
+/* 
+1) Para resolver um problema em programação precisamos definir:
+Quais são os dados de entrada
+Qual calculo será necessário para chegar a solução
+Qual será a saída no nosso algoritmo
+Defina quais são os dados de entrada, qual o cálculo e qual a saída dos problemas a
+seguir:
+a) João é frentista e atende 20 clientes por hora, em quantas horas ele atende 80
+clientes?
+Entrada: Clientes por hora, Clientes no total
+Cálculo: Clientes no total / clientes por hora
+Saída: Quantidade de horas para atender o total de clientes
+*/
+
+var hora = 20;
+var clientes = 80;
+console.log(clientes / hora);
+
+/*
+Resposta= 4
+*/
+
+/*
+Em uma fábrica trabalham 245 operários. Se cada um deles ganha 560 reais,
+quantos reais a fábrica paga por mês para todos os operários?
+*/
+
+totalOp = 245;
+salarioOp = 560;
+console.log(salarioOp * totalOp);
+
+/*
+Resposta= 137.200
+*/
+
+/*
+c) Em uma escola, estudam 1561 alunos. Para a Páscoa deste ano, a diretora vai
+distribuir uma cesta, com 15 doces dentro, para cada aluno. Quantos doces a
+diretora terá que comprar? E se cada cesta custar 5 reais, quanto ela irá gastar?
+*/
+
+alunosTotal = 1561;
+cesta = 15;
+valorCesta = 5;
+console.log(alunosTotal * cesta);
+console.log(alunosTotal * valorCesta);
+
+/*
+Respostas
+Toata de Doces = 23.415
+Gasto Toal = 7.805
+*/
+
+/*
+d) O Brasil tem 513 deputados federais. Suponha que cada um deles ganha um salário
+de 12379 reais por mês, quanto é pago por mês para todos os deputados? E por
+ano?
+*/
+
+deputados = 513;
+salario = 12379;
+ano = 12;
+console.log(deputados * salario);
+console.log(deputados * salario * ano);
+
+/*
+Respostas d
+Gasto mensal = 6350427
+Gasto Toal = 76205124
+*/
+
+/*
+e) Um canil possui 35 compartimentos para abrigar cães. Em cada um deles cabem 32
+cães. Quantos cães podem ser abrigados nesse canil? Os cuidados com cada cão
+custa 10 reais por mês. Quanto o canil gasta por ano para manter os cães?
+*/
---
+++ README.md
@@ -0,0 +1,33 @@
+### CappacitaDev Project
+
+<p align="center">
+  <img src= "./img/Cappacita.png" width="100%">
+</p>
+
+---
+
+- HTML
+- CSS
+- JS
+- JSON
+
+---
+
+  <footer class="container">
+            <div class="row text-center" id="footer-elementos">
+            </div>
+            </div>
+            <div class="row" style="text-align: center;margin-bottom: 10px;">
+                <div class="col-12">
+                    <small>© 2021 Jair Neto. All rights reserved <a href="https://www.linkedin.com/in/jair-monteiro-2a4a55aa/"
+                            target="_blank"></a>
+                            
+<p align="left">
+<a href="https://twitter.com/JairMonteiro" target="blank"><img align="center" src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/twitter.svg" alt="" height="30" width="40" /></a>
+<a href="https://www.linkedin.com/in/jair-monteiro-2a4a55aa/" target="blank"><img align="center" src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/linkedin.svg" alt="" height="30" width="40" /></a>
+<a href="your link" target="blank"><img align="center" src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/instagram.svg" alt="" height="30" width="40" /></a>
+<a href="https://www.youtube.com/channel/UC9ciqwerp6HMTNrmMBy3PZw" target="blank"><img align="center" src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/youtube.svg" alt="" height="30" width="40" /></a>
+</p>
+                </div>
+            </div>
+    </footer>
