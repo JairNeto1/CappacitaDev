13e14bed9f43d30e3e8692cc990c094fbc421051
JairNeto1 <jairmonteironeto@gmail.com>
2021-06-28 11:40:30 -0300

    new commit

--- DesafioMod_01/desafioModulo_01.js
+++ DesafioMod_01/desafioModulo_01.js
@@ -1,7 +1,6 @@
 retornaLista = (dadosClientes) => {
     for (var i = 0; i < dadosClientes.length; i++)
-    return dadosClientes[i].
-    }
+
     
 
 
@@ -35,10 +34,8 @@ retornaLista = (dadosClientes) => {
         aquisicao: "14/09/2020"
         }
         ]
-        Sua função deve retornar uma nova lista contendo apenas os clientes que possuem idade
-        igual ou maior que 18 e menor ou igual a 26.
-        Siga este modelo:
-        module.exports.retornaLista = (array) => {
-        //código
-        return "array final"
-        }
+    
+        return dadosClientes.length[i]
+    }        
+
+};    
