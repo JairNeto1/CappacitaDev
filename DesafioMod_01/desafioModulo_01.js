520c285b8a67c34d7a3714b694d3548ce04ffeb3
JairNeto1 <jairmonteironeto@gmail.com>
2021-06-30 19:08:40 -0300

    updating the desafioModulo

--- DesafioMod_01/desafioModulo_01.js
+++ DesafioMod_01/desafioModulo_01.js
@@ -1,41 +1,44 @@
-retornaLista = (dadosClientes) => {
-    for (var i = 0; i < dadosClientes.length; i++)
+var dadosClientes = [
+  {
+    nome: "Lucas",
+    sobrenome: "da Silva",
+    idade: 21,
+    genero: "Masculino",
+    plano: "full",
+    carencia: false,
+    aquisicao: "12/01/2019",
+  },
+  {
+    nome: "Ana",
+    sobrenome: "Lima",
+    idade: 17,
+    genero: "Feminino",
+    plano: "medium",
+    carencia: false,
+    aquisicao: "17/03/2019",
+  },
+  {
+    nome: "Adriana",
+    sobrenome: "Menezes",
+    idade: 27,
+    genero: "Feminino",
+    plano: "full",
+    carencia: true,
+    aquisicao: "14/09/2020",
+  },
+];
 
-    
+function retornaClientes(filtroIdade) {
+  var listaIdade = [];
+  var listaNome = [];
 
+  for (var i = 0; i < filtroIdade.length; i++) {
+    if (filtroIdade[i].idade >= 18 && filtroIdade[i].idade <= 26) {
+      listaIdade.push(filtroIdade[i]);
+      listaNome.push(filtroIdade[i]);
+    }
+  }
+  return listaNome;
+}
 
-
-    dadosClientes = [
-        {
-        nome: "Lucas",
-        sobrenome: "da Silva",
-        idade: 21,
-        genero: "Masculino",
-        plano: "full",
-        carencia: false,
-        aquisicao: "12/01/2019"
-        },
-        {
-        nome: "Ana",
-        sobrenome: "Lima",
-        idade: 17,
-        genero: "Feminino",
-        plano: "medium",
-        carencia: false,
-        aquisicao: "17/03/2019"
-        },
-        {
-        nome: "Adriana",
-        sobrenome: "Menezes",
-        idade: 27,
-        genero: "Feminino",
-        plano: "full",
-        carencia: true,
-        aquisicao: "14/09/2020"
-        }
-        ]
-    
-        return dadosClientes.length[i]
-    }        
-
-};    
+console.log(retornaClientes(dadosClientes));
