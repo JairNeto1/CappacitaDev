a5a074988e783836f1840f9a407ccdb06f4a7668
JairNeto1 <jairmonteironeto@gmail.com>
2021-08-19 09:22:36 -0300

    iniciating module 3

---
+++ modulo3/src/dataBase.js
@@ -0,0 +1,11 @@
+const sequence = {
+  _id: 1,
+  get id() { return this._id++}
+}
+
+const pokemons = {}
+
+function salvarPokenmons(pokemon) {
+  if(!pokemon.id) pokemon.id =sequence.id
+  pokemon[]
+}
